f64d54dd5892fa31c546a7f925fc7799148a4b30
JulianaCBB1 <julianacbbuzanello@gmail.com>
2026-04-29 19:55:39 +0200

    added typeorm data source

---
+++ api/src/data-source.ts
@@ -0,0 +1,8 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL as string,
+  synchronize: false,
+  logging: false,
+});
