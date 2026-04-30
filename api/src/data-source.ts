a0c5a53b8ab8b6e589c3464093adae47eac37b3d
JulianaCBB1 <julianacbbuzanello@gmail.com>
2026-04-30 14:59:57 +0200

    added User entity and db table

--- api/src/data-source.ts
+++ api/src/data-source.ts
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(__dirname, '../.env') });
 import { DataSource } from 'typeorm';
+import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL as string,
+  entities: [User],
+  migrations: [path.join(__dirname, 'migrations', '*.ts')],
   synchronize: false,
   logging: false,
 });
